4e12638074803fe842b3da280db7233a79801c96
snowa-snowa <sione8172635@gmail.com>
2025-07-19 10:08:24 +0500

    deploy

--- DJ_Channels_01/settings.py
+++ DJ_Channels_01/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['telegramm-channels.onrender.com', '127.0.0.1', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['telegramm-channels.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://telegramm-channels.onrender.com']
 
 # Application definition
 
